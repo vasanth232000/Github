7c560462ccc7aabc7bcc791259d9b9f593fa00e1
vasanth232000 <103664377+vasanth232000@users.noreply.github.com>
2023-01-30 23:59:50 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+{
+  path:"/",
+  element:<span>HOME</span>
+},
+{
+  path:"/products/:id",
+  element:<span>CATEGORIES</span>
+},
+{
+  path:"/product/:id",
+  element:<span>PRODUCTS</span>
+}
+])
+
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router} />
     </div>
   );
 }
